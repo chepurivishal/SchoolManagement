8df10496711046951da904324491722b6933261e
chepurivishal <vishalvishu567@gmail.com>
2020-09-03 10:46:48 +0530

    Changes mongo connection

--- connection/index.js
+++ connection/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-var Url = process.env.MONGODB_URI || "mongodb+srv://admin:admin@123@cluster0.0m68l.mongodb.net/<SchoolManagment>?retryWrites=true&w=majority";
+var Url = "mongodb+srv://admin:admin@123@cluster0.0m68l.mongodb.net/<SchoolManagment>?retryWrites=true&w=majority";
 
 var connectDB = async()=> {
     await mongoose.connect(Url,{useUnifiedTopology: true, useNewUrlParser : true} )
